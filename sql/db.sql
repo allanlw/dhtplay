a9fb0bae980db9205d82032e46b0619fc5e8e39c
Allan Wirth <allanlw@gmail.com>
2011-08-14 23:19:40 -0400

    Added some documentation and indecies to the database.

--- sql/db.sql
+++ sql/db.sql
@@ -1,4 +1,8 @@
+/* This SQL script for SQLite generates the tables for DHTPlay.
+ * It depends upon custom types that are registered by the python connection.
+ */
 PRAGMA foreign_keys = on;
+
 CREATE TABLE IF NOT EXISTS buckets (
   id INTEGER PRIMARY KEY NOT NULL,
   start sha1hash NOT NULL,
@@ -6,10 +10,11 @@ CREATE TABLE IF NOT EXISTS buckets (
   created timestamp NOT NULL,
   updated timestamp NOT NULL
 );
+
 CREATE TABLE IF NOT EXISTS nodes (
   id INTEGER PRIMARY KEY NOT NULL,
-  hash sha1hash NOT NULL,
-  contact contactinfo NOT NULL,
+  hash sha1hash UNIQUE NOT NULL,
+  contact contactinfo UNIQUE NOT NULL,
   bucket_id INTEGER NOT NULL,
   good BOOLEAN NOT NULL,
   pending BOOLEAN NOT NULL,
@@ -20,12 +25,18 @@ CREATE TABLE IF NOT EXISTS nodes (
 
   FOREIGN KEY(bucket_id) REFERENCES buckets(id)
 );
+CREATE UNIQUE INDEX IF NOT EXISTS nodes_hash ON nodes(hash);
+CREATE UNIQUE INDEX IF NOT EXISTS nodes_contact ON nodes(contact);
+CREATE INDEX IF NOT EXISTS nodes_bucket_id ON nodes(bucket_id);
+
 CREATE TABLE IF NOT EXISTS peers (
   id INTEGER PRIMARY KEY NOT NULL,
   contact contactinfo UNIQUE NOT NULL,
   created timestamp NOT NULL,
   updated timestamp NOT NULL
 );
+CREATE UNIQUE INDEX IF NOT EXISTS peers_contact ON peers(contact);
+
 CREATE TABLE IF NOT EXISTS torrents (
   id INTEGER PRIMARY KEY NOT NULL,
   hash sha1hash UNIQUE NOT NULL,
@@ -34,6 +45,8 @@ CREATE TABLE IF NOT EXISTS torrents (
   seeds bloom NOT NULL,
   peers bloom NOT NULL
 );
+CREATE UNIQUE INDEX IF NOT EXISTS torrents_hash ON torrents(hash);
+
 CREATE TABLE IF NOT EXISTS peer_torrents (
   id INTEGER PRIMARY KEY NOT NULL,
   peer_id INTEGER NOT NULL,
@@ -45,3 +58,5 @@ CREATE TABLE IF NOT EXISTS peer_torrents (
   FOREIGN KEY(peer_id) REFERENCES peers(id),
   FOREIGN KEY(torrent_id) REFERENCES torrents(id)
 );
+CREATE INDEX IF NOT EXISTS peer_torrents_peer_id ON peer_torrents(peer_id);
+CREATE INDEX IF NOT EXISTS peer_torrents_torrent_id ON peer_torrents(torrent_id);
